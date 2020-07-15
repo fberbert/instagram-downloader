0b45300fe0365dc6b37ea8d11c7145153523f1b0
Fábio Berbert de Paula <fberbert@gmail.com>
2020-07-15 02:15:36 -0300

    passar URL via sys.argv

--- downloader.py
+++ downloader.py
@@ -1,6 +1,19 @@
 import instaloader
 from getpass import getpass
 import re
+import os
+import sys
+
+#verificar se a URL foi informada
+try:
+    url = sys.argv[1]
+except IndexError:
+    print("Forma de uso:\n\n", sys.argv[0], "URL\n\nInforme uma URL válida\n\n")
+    sys.exit()
+
+#diretório de download
+downloadDir = '/home/fabio/Downloads'
+os.chdir(downloadDir)
 
 loader = instaloader.Instaloader(
   download_pictures=True, 
@@ -17,16 +30,16 @@ loader = instaloader.Instaloader(
 #caso de download de posts de contas privadas
 #comente as próximas 3 linhas caso esteja
 #fazendo download de posts de contas públicas
-username = input('Usuário: ')
-password = getpass('Senha: ')
-loader.login(username,password)
+# username = input('Usuário: ')
+# password = getpass('Senha: ')
+# loader.login(username,password)
 
-url = input('URL: ')
-# https://www.instagram.com/p/CCfeQV-AssF/
+#extrair "short_code" da URL
+#exemplo: https://www.instagram.com/p/CCfeQV-AssF/
 expr = r'\/p\/([^\/]*)/'
 found = re.search(expr, url)
 
 if found:
   print("Baixando ", found.group(1), "...")
   post = instaloader.Post.from_shortcode(loader.context, found.group(1))
-  loader.download_post(post, "Downloads")
+  loader.download_post(post, ".")
